6ec480e253f1325cc4639b5e97e37853cdf6faf8
allan716 <525223688@qq.com>
2021-03-17 11:03:29 +0800

    update detector_test.go
    Signed-off-by: allan716 <525223688@qq.com>

--- detector_test.go
+++ detector_test.go
@@ -10,6 +10,12 @@ func TestNewDetector(t *testing.T) {
 
 func TestDetector_RDPCheck(t *testing.T) {
 	det := NewDetector()
+	if det.rdp.GetVersion() == ""{
+		t.Fatal("rdp version is empty")
+	}
+	if len(det.rdp.GetSupportOSVersion()) == 0{
+		t.Fatal("rdp Support OS Version is empty")
+	}
 	// change to your PC IP and port
 	err := det.RDPCheck("192.168.200.24", "3389")
 	if err != nil {
@@ -20,6 +26,9 @@ func TestDetector_RDPCheck(t *testing.T) {
 func TestDetector_SSHCheck(t *testing.T) {
 
 	det := NewDetector()
+	if det.ssh.GetVersion() == ""{
+		t.Fatal("ssh version is empty")
+	}
 	// change to your PC IP and port
 	err := det.SSHCheck("192.168.200.23", "22")
 	if err != nil {
